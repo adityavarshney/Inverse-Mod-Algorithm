820618c146115d6ee8c3b0a58a707919e72c091d
Aditya Varshney <adityavarshney@gmail.com>
2017-02-15 13:34:17 -0800

    Update extended_euclid.py
    Adjusted to handle negative results.

--- extended_euclid.py
+++ extended_euclid.py
@@ -5,7 +5,10 @@ def extended_gcd(x,y):
 		(d, a, b) = extended_gcd(y, x % y)
 		return (d, b, a - x//y * b)
 
-print("a (mod b)")
-x = int(input("Enter a. \t"))
-y = int(input("Enter b. \t"))
-print(extended_gcd(x,y)[1])
+print("Find a^-1 in (mod b)")
+x = int(input("Enter a *the value to be inverted*. \t"))
+y = int(input("Enter b *the modular base*. \t"))
+answer = extended_gcd(x,y)[1]
+if answer < 0:
+	answer += y 
+print("Inverse to " + str(x) + " in mod " + str(y) + " is " + str(answer) + ".") 
